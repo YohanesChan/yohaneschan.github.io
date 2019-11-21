f215f2715533c2101c445a919779022774b19fbe
YohanesChan <chandrayh24@gmail.com>
2019-11-21 14:53:39 +0700

    inital

---
+++ precache-manifest.c7bfc711d3c79d277e33e18d70da6c23.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dac76d13dcfc0fc5d0e",
+    "url": "/css/about.3b3c9ddb.css"
+  },
+  {
+    "revision": "aea0949ba0ae84c22ef2",
+    "url": "/css/chunk-vendors.ec4de8ca.css"
+  },
+  {
+    "revision": "6b3884d946a5efe38261724a8a783e17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1dac76d13dcfc0fc5d0e",
+    "url": "/js/about.d9684221.js"
+  },
+  {
+    "revision": "73c4dcfc0299e4817f68",
+    "url": "/js/app.fc84af41.js"
+  },
+  {
+    "revision": "aea0949ba0ae84c22ef2",
+    "url": "/js/chunk-vendors.ac7a7141.js"
+  },
+  {
+    "revision": "a900cbb5bac787121a0f0c93e5ce9fb6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
